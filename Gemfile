79b8b8a7a62a5dc284acd26055fda394d27f6684
Matthew Flores <flo2312@gmail.com>
2019-07-07 23:32:32 +0000

    WK5LS30COMPLETE

--- Gemfile
+++ Gemfile
@@ -78,3 +78,7 @@ gem 'will_paginate', '~>3.1.0'
 gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
 
 gem 'devise'
+
+gem 'geocoder'
+
+gem 'figaro'
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+  
   validates :name, presence: true, length: { minimum: 3, too_short: "%{count} characters is the minimum allowed" }
   validates :address, presence: true
   validates :description, presence: true
