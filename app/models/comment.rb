3af652d8348e745b6b14990e467d8231e0e90f46
Matthew Flores <flo2312@gmail.com>
2019-07-08 03:04:40 +0000

    WK5LS36COMPLETE

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    'One star': '1_star',
+    'Two stars': '2_stars',
+    'Three stars': '3_stars',
+    'Four stars': '4_stars',
+    'Five stars': '5_stars'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end  
 end
