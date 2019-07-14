887860e21cbc9f574c06d75066b4ee83e3ec2275
Matthew Flores <flo2312@gmail.com>
2019-07-14 19:04:42 +0000

    WK6LS41COMPLETE

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: 'no-reply@nomsterapp.com'
 
   def comment_added
-    mail(to: "flo2312@gmail.com",
+    mail(to: "gryffinatorandtobifer@gmail.com",
         subject: "A comment has been added to your place")
   end
 end
